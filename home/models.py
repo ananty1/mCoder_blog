2fe9d0577960480806eceb82be49c7acff85dc88
anant <ananty@iitbhilai.ac.in>
2023-07-19 06:36:07 +0530

    5th commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=300)
+    phone = models.CharField(max_length=13)
+    email = models.CharField(max_length=40)
+
+    content = models.TextField()
+
