318b650aa5c75ab332fcf64a2a28c2d1fbfcc004
anant <ananty@iitbhilai.ac.in>
2023-07-20 14:36:16 +0530

    7th commit

--- blog/models.py
+++ blog/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class Contact(models.Model):
+class Post(models.Model):
     sno = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=300)
-    phone = models.CharField(max_length=13)
-    email = models.CharField(max_length=40)
+    title = models.CharField(max_length=80)
+    author = models.CharField(max_length=50)
     content = models.TextField()
-    timestamp = models.DateTimeField(auto_now=True,blank=True)
+    slug = models.CharField(max_length=130)
+    timestamp = models.DateTimeField(blank=True)
 
 
     def __str__(self):
-        return f"{self.sno}){self.name}"
+        return f"{self.sno}){self.title}"
     
 
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 # Create your views here.
 
 def blogHome(request):
-    return render(request,'blog/blogHome.html')
+    blogs = Post.objects.all().order_by('sno')[:50]
+    context = {'blogs':blogs}
+
+    return render(request,'blog/blogHome.html',context)
 
 def blogPost(request,slug):
     # return HttpResponse(f"Welcome back to our blogPost: {slug}")
