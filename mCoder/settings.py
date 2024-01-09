e1bd755b1f4c8198a7c2315f270c337130fb0707
anant <ananty@iitbhilai.ac.in>
2024-01-10 00:23:35 +0530

    made some vercel-deployment changes ready for deployment:

--- mCoder/settings.py
+++ mCoder/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants as messages
+import os 
+from decouple import config
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c2zse_q&7fy!-*g&1^g^hy587fz%-mopry67nishp+n%jb3iw='
+SECRET_KEY = 'c2zse_q&7fy!-*g&1^g^hy587fz%-mopry67nishp+n%jb3iw'
+# SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
